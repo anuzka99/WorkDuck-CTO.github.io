60ddaee2e1cc0ed1400d3aad3d68e6c1007e23ad
WorkDuck-CTO <ruddhi@workduck.in>
2020-04-18 14:42:46 +0530

    major changes

--- script.js
+++ script.js
@@ -13,13 +13,12 @@ $(document).ready(function(){
       // Using jQuery's animate() method to add smooth page scroll
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
-        scrollTop: $(hash).offset().top
+        scrollTop: $(hash).offset().top - 100
       }, 500, function(){
-   
         // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
+        // window.location.hash = hash;
       });
-    } // End if
+     } // End if
   });
 });
 
@@ -96,10 +95,6 @@ $cell.find('.js-expander').click(function() {
     } else {
       $cell.not($thisCell).addClass('is-inactive');
     }
-
-  } else {
-    $thisCell.removeClass('is-expanded').addClass('is-collapsed');
-    $cell.not($thisCell).removeClass('is-inactive');
   }
 });
 
