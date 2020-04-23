ffe02f9fb0387431fc7a7a0a058f06cb7989b008
WorkDuck-CTO <ruddhi@workduck.in>
2020-04-23 11:27:13 +0530

    Deleted unwanted

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,7 @@ $(function () {
         var scrollbarLocation = $(this).scrollTop();
 
         scrollLink.each(function () {
-
+            console.log($(this.hash).offset());
             var sectionOffset = $(this.hash).offset().top - 73;
 
             if (sectionOffset <= scrollbarLocation) {
@@ -140,47 +140,6 @@ $(function () {
         delay: 10,
         time: 3000
     });
-
-
-    //===== testimonial active
-
-    $('.testimonial-active').slick({
-        dots: true,
-        speed: 800,
-        arrows: false,
-        centerMode: true,
-        centerPadding: "0",
-        slidesToShow: 3,
-        slidesToScroll: 4,
-        responsive: [
-            {
-                breakpoint: 1200,
-                settings: {
-                    slidesToShow: 3,
-                }
-            },
-            {
-                breakpoint: 992,
-                settings: {
-                    slidesToShow: 2,
-                    centerMode: false,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-            {
-                breakpoint: 576,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
-    });
-
 
     //===== Back to top
 
